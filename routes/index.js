cd1e011e620ea3a9ea6829bbc6e48f02b8b57218
Joe Blau <josephblau@gmail.com>
2014-03-26 18:00:19 -0700

    Style Cleanup

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@
 var Firebase = require('firebase');
 var crypto = require('crypto');
 
-
-
 exports.index = function(req, res){
 
   var clientIP = req.headers['x-forwarded-for'] || req.ip;
@@ -18,6 +16,6 @@ exports.index = function(req, res){
   var location = shasum.digest('hex');
 
   var myRootRef = new Firebase('https://otto.firebaseio.com/');
-  myRootRef.child(location).set({on:true, h:0.6078125, s:0.6690142, b :1, a:1});
-  res.render('index', { title: 'Express', loc: location });
+  myRootRef.child(location).set({on:true, h:0, s:0, b :1, a:1});
+  res.render('index', { loc: location });
 };
